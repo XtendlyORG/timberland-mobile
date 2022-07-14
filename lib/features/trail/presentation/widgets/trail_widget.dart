68dbf2d7a95c75ea45f55ff6e3edf4addde176e1
markcabalona <markcabalona58@gmail.com>
2022-07-14 09:52:26 +0800

    added default values on error

--- lib/features/trail/presentation/widgets/trail_widget.dart
+++ lib/features/trail/presentation/widgets/trail_widget.dart
@@ -37,21 +37,28 @@ class TrailWidget extends StatelessWidget {
                   fit: BoxFit.fitWidth),
             ),
           ),
-          Text.rich(TextSpan(children: [
-            TextSpan(
-                text: '${trail.difficulty.name}\n',
-                style: TextStyle(
-                  color: trail.difficulty.textColor,
-                )),
-            TextSpan(
-              text: '${trail.trailName}\n',
-              style: Theme.of(context).textTheme.titleMedium,
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                      text: '${trail.difficulty.name}\n',
+                      style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                            color: trail.difficulty.textColor,
+                          )),
+                  TextSpan(
+                    text: '${trail.trailName}\n',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  TextSpan(
+                    text: trail.location,
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                ],
+              ),
             ),
-            TextSpan(
-              text: trail.location,
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
-          ])),
+          ),
           TrailSpecs(trail: trail)
         ],
       ),
