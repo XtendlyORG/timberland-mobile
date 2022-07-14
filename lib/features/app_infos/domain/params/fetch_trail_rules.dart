620dea75560c1802a1ba5fb15201127bfbd54049
markcabalona <markcabalona58@gmail.com>
2022-07-14 09:10:07 +0800

    app infos params

---
+++ lib/features/app_infos/domain/params/fetch_trail_rules.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dartz/dartz.dart';
+
+import 'package:timberland_biketrail/core/errors/failures.dart';
+import 'package:timberland_biketrail/core/utils/usecase.dart';
+import 'package:timberland_biketrail/features/app_infos/domain/entities/trail_rule.dart';
+import 'package:timberland_biketrail/features/app_infos/domain/repositories/app_infos_repository.dart';
+import 'package:timberland_biketrail/features/trail/domain/entities/trail.dart';
+
+class FetchTrailRules implements Usecase<List<TrailRule>, void> {
+  @override
+  final AppInfoRepository repository;
+  const FetchTrailRules({
+    required this.repository,
+  });
+  @override
+  Future<Either<AppInfoFailure, List<TrailRule>>> call(void params) {
+    return repository.fetchTrailRules();
+  }
+}
