608520fbcb5a779151523c330f9e3d31e2305f3e
markcabalona <markcabalona58@gmail.com>
2022-07-14 16:52:52 +0800

    added trail-detail page

--- lib/features/trail/presentation/widgets/trail_widget.dart
+++ lib/features/trail/presentation/widgets/trail_widget.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
 import 'package:timberland_biketrail/core/router/router.dart';
 import 'package:timberland_biketrail/features/trail/domain/entities/trail.dart';
+import 'package:timberland_biketrail/features/trail/presentation/widgets/trail_specs.dart';
 
 class TrailWidget extends StatelessWidget {
   final Trail trail;
@@ -60,74 +60,6 @@ class TrailWidget extends StatelessWidget {
             ),
           ),
           TrailSpecs(trail: trail)
-        ],
-      ),
-    );
-  }
-}
-
-class TrailSpecs extends StatelessWidget {
-  const TrailSpecs({
-    Key? key,
-    required this.trail,
-    this.maxWidth = 400,
-  }) : super(key: key);
-
-  final Trail trail;
-  final double maxWidth;
-
-  @override
-  Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(maxWidth: 400),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text.rich(
-            TextSpan(
-              children: [
-                TextSpan(
-                  text: 'Length\n',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                TextSpan(
-                  text: '${trail.length} mi',
-                  style: Theme.of(context).textTheme.subtitle2,
-                ),
-              ],
-            ),
-            textAlign: TextAlign.start,
-          ),
-          Text.rich(
-            TextSpan(
-              children: [
-                TextSpan(
-                  text: 'Elevation Gain\n',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                TextSpan(
-                  text: '${trail.length} ft',
-                  style: Theme.of(context).textTheme.subtitle2,
-                ),
-              ],
-            ),
-            textAlign: TextAlign.start,
-          ),
-          Text.rich(
-            TextSpan(
-              children: [
-                TextSpan(
-                  text: 'Route Type\n',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                TextSpan(
-                  text: trail.routeType,
-                  style: Theme.of(context).textTheme.subtitle2,
-                ),
-              ],
-            ),
-            textAlign: TextAlign.start,
-          ),
         ],
       ),
     );
