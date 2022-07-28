5ae6b081d5068d5d61b8de5ac5ecdfcf345f58d0
markcabalona <markcabalona58@gmail.com>
2022-07-28 18:21:15 +0800

    added bullet form in trail rules

--- lib/features/app_infos/presentation/widgets/trail_rule_widget.dart
+++ lib/features/app_infos/presentation/widgets/trail_rule_widget.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:timberland_biketrail/core/constants/constants.dart';
 import 'package:timberland_biketrail/features/app_infos/domain/entities/trail_rule.dart';
 
 class TrailRuleWidget extends StatelessWidget {
@@ -11,16 +12,33 @@ class TrailRuleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text.rich(
-      TextSpan(
-        children: [
-          TextSpan(
-            text: trailRule.note.trim(),
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          '\u2022',
+          style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        const SizedBox(
+          width: kVerticalPadding,
+        ),
+        Expanded(
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: trailRule.note.trim(),
+                ),
+              ],
+            ),
+            style: Theme.of(context).textTheme.titleSmall,
+            textAlign: TextAlign.justify,
           ),
-        ],
-      ),
-      style: Theme.of(context).textTheme.titleSmall,
-      textAlign: TextAlign.justify,
+        ),
+      ],
     );
   }
 }
