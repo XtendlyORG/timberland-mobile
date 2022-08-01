d99076db584f38aa1e68df7a057e76b2f38beb56
markcabalona <markcabalona58@gmail.com>
2022-08-01 17:21:55 +0800

    added booking to navbar

--- lib/features/booking/presentation/pages/booking_page.dart
+++ lib/features/booking/presentation/pages/booking_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:timberland_biketrail/core/constants/constants.dart';
 import 'package:timberland_biketrail/core/presentation/widgets/inherited_widgets/inherited_trail.dart';
+import 'package:timberland_biketrail/core/presentation/widgets/refreshable_scrollview.dart';
 import 'package:timberland_biketrail/core/presentation/widgets/timberland_scaffold.dart';
 import 'package:timberland_biketrail/features/authentication/presentation/bloc/auth_bloc.dart';
 import 'package:timberland_biketrail/features/booking/presentation/widgets/booking_form.dart';
@@ -15,13 +16,19 @@ class BookingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TimberlandScaffold(
-      disableBackButton: !Navigator.canPop(context),
-      titleText: "Booking Form",
-      body: Padding(
+    return RefreshableScrollView(
+      onRefresh: () async {},
+      child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
         child: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.only(top: kToolbarHeight, bottom: 10),
+              child: AutoSizeText(
+                'Booking Form',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(
                 horizontal: kHorizontalPadding,
