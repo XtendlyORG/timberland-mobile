f36ae181638a0fdb7c0d6c94eb4896a9c971362e
markcabalona <markcabalona58@gmail.com>
2022-08-05 09:11:24 +0800

    booking history redesign

--- lib/features/booking/domain/entities/booking.dart
+++ lib/features/booking/domain/entities/booking.dart
@@ -1,15 +1,20 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 class Booking extends Equatable {
   final String bookingId;
   final DateTime bookDate;
+  final TimeOfDay startTime;
+  final TimeOfDay endTime;
   final String serviceId;
   final String userId;
   final String status;
   const Booking({
     required this.bookingId,
     required this.bookDate,
+    required this.startTime,
+    required this.endTime,
     required this.serviceId,
     required this.userId,
     required this.status,
