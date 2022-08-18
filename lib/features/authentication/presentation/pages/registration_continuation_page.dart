6f1bdff2585fee374d25918d6d691b8e5b93e8d1
markcabalona <markcabalona58@gmail.com>
2022-08-18 10:32:41 +0800

    refactored navigation in auth pages

--- lib/features/authentication/presentation/pages/registration_continuation_page.dart
+++ lib/features/authentication/presentation/pages/registration_continuation_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:timberland_biketrail/core/constants/constants.dart';
+import 'package:timberland_biketrail/core/router/router.dart';
 import 'package:timberland_biketrail/features/authentication/presentation/widgets/auth_page_container.dart';
 import 'package:timberland_biketrail/features/authentication/presentation/widgets/registration_form_continuation.dart';
 
@@ -11,36 +13,50 @@ class RegistrationContinuationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            foregroundColor: Theme.of(context).colorScheme.primary,
-            elevation: 0,
-            leading: Tooltip(
-              message: 'Back',
-              child: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: const Icon(
-                  Icons.arrow_back_rounded,
-                  color: Colors.black,
+    return WillPopScope(
+      onWillPop: () async {
+        if (Navigator.canPop(context)) {
+          Navigator.pop(context);
+        } else {
+          context.goNamed(Routes.register.name);
+        }
+        return false;
+      },
+      child: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              foregroundColor: Theme.of(context).colorScheme.primary,
+              elevation: 0,
+              leading: Tooltip(
+                message: 'Back',
+                child: IconButton(
+                  onPressed: () {
+                    if (Navigator.canPop(context)) {
+                      Navigator.pop(context);
+                    } else {
+                      context.goNamed(Routes.register.name);
+                    }
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back_rounded,
+                    color: Colors.black,
+                  ),
                 ),
               ),
             ),
-          ),
-          extendBodyBehindAppBar: true,
-          body: AuthPageContainer(
-            child: Column(
-              children: const [
-                RegistrationContinuationForm(),
-                SizedBox(
-                  height: kVerticalPadding,
-                ),
-              ],
+            extendBodyBehindAppBar: true,
+            body: AuthPageContainer(
+              child: Column(
+                children: const [
+                  RegistrationContinuationForm(),
+                  SizedBox(
+                    height: kVerticalPadding,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
