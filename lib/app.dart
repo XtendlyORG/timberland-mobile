cd5ce96c0069c89deff8c281d1e4cb37ca2d53c4
markcabalona <markcabalona58@gmail.com>
2022-08-18 12:24:34 +0800

    added internet connection checker

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,15 @@
 import 'dart:developer';
 
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:timberland_biketrail/core/constants/constants.dart';
+import 'package:timberland_biketrail/core/themes/timberland_color.dart';
+import 'package:timberland_biketrail/core/utils/internet_connection.dart';
 
 import 'core/router/app_router.dart';
 import 'core/themes/timberland_theme.dart';
@@ -25,6 +30,7 @@ Future<void> run({
   await dotenv.load(fileName: dotEnvFileName);
 
   await Session().init();
+  await InternetConnectivity().init();
 
   runApp(MultiBlocProvider(
     providers: [
@@ -56,11 +62,72 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late final GlobalKey<ScaffoldMessengerState> _messengerKey;
   @override
   void initState() {
     super.initState();
     final Session session = Session();
     FlutterNativeSplash.remove();
+
+    final InternetConnectivity internetConnectivity = InternetConnectivity();
+    _messengerKey = GlobalKey<ScaffoldMessengerState>();
+
+    if (!internetConnectivity.internetConnected) {
+      SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+        _messengerKey.currentState!.showSnackBar(
+          SnackBar(
+            duration: const Duration(days: 1),
+            dismissDirection: DismissDirection.none,
+            content: const AutoSizeText(
+              'No internet connection',
+              textAlign: TextAlign.center,
+            ),
+            behavior: SnackBarBehavior.floating,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(24),
+            ),
+            margin: const EdgeInsets.only(
+              right: 20,
+              left: 20,
+              bottom: kHorizontalPadding,
+            ),
+          ),
+        );
+      });
+    }
+
+    internetConnectivity.addListener(() async {
+      log("adasd");
+      log(internetConnectivity.internetConnected.toString());
+      if (!internetConnectivity.internetConnected) {
+        log("No internet");
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          _messengerKey.currentState!.showSnackBar(
+            SnackBar(
+              duration: const Duration(days: 1),
+              dismissDirection: DismissDirection.none,
+              content: const AutoSizeText(
+                'No internet connection',
+                textAlign: TextAlign.center,
+              ),
+              behavior: SnackBarBehavior.floating,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24),
+              ),
+              margin: const EdgeInsets.only(
+                right: 20,
+                left: 20,
+                bottom: kHorizontalPadding,
+              ),
+            ),
+          );
+        });
+      } else {
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          _messengerKey.currentState?.clearMaterialBanners();
+        });
+      }
+    });
 
     BlocProvider.of<AuthBloc>(context).stream.listen((state) {
       if (state is AuthError) {
@@ -76,6 +143,12 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
+  void dispose() {
+    InternetConnectivity().removeListener(() {});
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Listener(
       onPointerDown: (_) {
@@ -86,6 +159,7 @@ class _MyAppState extends State<MyApp> {
         }
       },
       child: MaterialApp.router(
+        scaffoldMessengerKey: _messengerKey,
         title: 'Timberland Mountain BikeTrail',
         debugShowCheckedModeBanner: false,
         theme: TimberlandTheme.lightTheme,
