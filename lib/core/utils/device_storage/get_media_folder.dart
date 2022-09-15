11929ce63f5a38a8c6644d660c55d6dd433d9412
Mark Cabalona <user@users-MacBook-Pro.local>
2022-09-15 09:19:34 +0800

    ios support for downloading trail map

--- lib/core/utils/device_storage/get_media_folder.dart
+++ lib/core/utils/device_storage/get_media_folder.dart
@@ -1,9 +1,14 @@
 import 'dart:io';
 
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 Future<String> getPhotoDirectory(String directory) async {
-  final dir = Directory('storage/emulated/0/Pictures/$directory');
+  final dir = Platform.isAndroid
+      ? Directory('storage/emulated/0/Pictures/$directory')
+      : Directory(
+          '${(await getApplicationDocumentsDirectory()).path}Pictures/$directory',
+        );
   final status = await Permission.manageExternalStorage.status;
   if (!status.isGranted && !status.isRestricted) {
     await Permission.manageExternalStorage.request();
