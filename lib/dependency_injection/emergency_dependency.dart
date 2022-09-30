e0251f3104bd2c033f96782993100ca6f98a0d4e
Mark Cabalona <markcabalona58@gmail.com>
2022-09-30 11:37:30 +0800

    emergency dependency injection

---
+++ lib/dependency_injection/emergency_dependency.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:timberland_biketrail/features/emergency/data/datasources/emergency_datasource_impl.dart';
+import 'package:timberland_biketrail/features/emergency/data/repositories/emergency_repository_impl.dart';
+import 'package:timberland_biketrail/features/emergency/domain/repositories/emergency_repository.dart';
+import 'package:timberland_biketrail/features/emergency/presentation/bloc/emergency_bloc.dart';
+
+import '../core/configs/environment_configs.dart';
+
+final serviceLocator = GetIt.instance;
+void init() {
+  serviceLocator.registerFactory<EmergencyBloc>(
+    () => EmergencyBloc(repository: serviceLocator()),
+  );
+
+  serviceLocator.registerLazySingleton<EmergencyRepository>(
+    () => EmergencyRepositoryImpl(
+      dataSource: EmergencyDataSourceImpl(
+        environmentConfig: serviceLocator<EnvironmentConfig>(),
+        dioClient: serviceLocator<Dio>(),
+      ),
+    ),
+  );
+}
