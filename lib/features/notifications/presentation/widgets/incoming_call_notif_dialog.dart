5532371e628909f597dd62b3ff232ab48d107ff8
markcabalona <markcabalona58@gmail.com>
2022-12-02 16:10:51 +0800

    Emergency Incoming Notification - Added incoming call notificaiton dialog     - answer and decline button

---
+++ lib/features/notifications/presentation/widgets/incoming_call_notif_dialog.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+import 'package:go_router/go_router.dart';
+import 'package:timberland_biketrail/core/constants/padding.dart';
+import 'package:timberland_biketrail/core/presentation/widgets/widgets.dart';
+import 'package:timberland_biketrail/core/router/router.dart';
+import 'package:timberland_biketrail/core/themes/timberland_color.dart';
+import 'package:timberland_biketrail/features/emergency/presentation/pages/emergency_page.dart';
+import 'package:vibration/vibration.dart';
+
+class IncomingCallNotifDialog extends StatelessWidget {
+  const IncomingCallNotifDialog({
+    Key? key,
+    required this.incomingCallNotifCtrl,
+  }) : super(key: key);
+
+  final AnimationController incomingCallNotifCtrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        top: kToolbarHeight / 2,
+        bottom: kToolbarHeight / 4,
+        left: kVerticalPadding,
+        right: kVerticalPadding,
+      ),
+      margin: const EdgeInsets.symmetric(
+        horizontal: kVerticalPadding,
+      ),
+      decoration: BoxDecoration(
+        color: TimberlandColor.background,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+              color: Colors.black26,
+              blurRadius: 5,
+              spreadRadius: 5,
+              offset: Offset(1, 1))
+        ],
+      ),
+      clipBehavior: Clip.hardEdge,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'TMBT Admin is calling you...',
+            style:
+                Theme.of(context).textTheme.bodyLarge?.copyWith(fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: kVerticalPadding,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  color: Theme.of(context).disabledColor,
+                ),
+              ),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: FilledTextButton(
+                    onPressed: () {
+                      incomingCallNotifCtrl.reverse();
+                      Vibration.cancel();
+                      FlutterRingtonePlayer.stop();
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: TimberlandColor.lightRed,
+                      foregroundColor: TimberlandColor.background,
+                    ),
+                    child: const Icon(Icons.call_end),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                  child: VerticalDivider(
+                    thickness: 1.5,
+                    color: Theme.of(context).disabledColor,
+                  ),
+                ),
+                Expanded(
+                  child: FilledTextButton(
+                    onPressed: () {
+                      incomingCallNotifCtrl.reverse();
+                      context.pushNamed(
+                        Routes.emergency.name,
+                        extra: CallDirection.incoming,
+                      );
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: TimberlandColor.accentColor,
+                      foregroundColor: TimberlandColor.background,
+                    ),
+                    child: const Icon(Icons.call),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
