34d2c2793d87caefd6a7623623caeca4bcfdf06e
markcabalona <markcabalona58@gmail.com>
2022-12-04 12:37:22 +0800

    BloodType Form Field - Updated Blood Type form Field     - changed it to a drop down

---
+++ lib/core/constants/bloodtype_dropdown_items.dart
@@ -0,0 +1,10 @@
+const kBloodTypes = [
+  'A Positive (A+)',
+  'A Negative (A-)',
+  'B Positive (B+)',
+  'B Negative (B-',
+  'O Positive (O+)',
+  'O Negative (O-)',
+  'AB Positive (AB+)',
+  'AB Negative (AB-)',
+];
