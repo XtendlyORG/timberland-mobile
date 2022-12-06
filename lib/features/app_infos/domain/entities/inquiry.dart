f21c380f3b50eaa927bdac43f77839d0df0e9048
markcabalona <markcabalona58@gmail.com>
2022-12-06 16:24:34 +0800

    updated default values for inquiry params

--- lib/features/app_infos/domain/entities/inquiry.dart
+++ lib/features/app_infos/domain/entities/inquiry.dart
@@ -33,8 +33,8 @@ class Inquiry extends Equatable {
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'email': email,
-      'firstname': firstName.isEmpty ? null : firstName,
-      'lastname': lastName.isEmpty ? null : lastName,
+      'firstname': firstName,
+      'lastname': lastName,
       'subject': subject,
       'message': message,
     };
