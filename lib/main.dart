af8e8e9ca331c160175e033dcd9102027870db5a
markcabalona <markcabalona58@gmail.com>
2023-01-05 09:47:56 +0800

    Android 6 http calls support

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
+import 'dart:io';
+
 import 'app.dart' as app;
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 Future<void> main() async {
+  HttpOverrides.global = MyHttpOverrides();
   await app.run(dotEnvFileName: ".env");
 }
