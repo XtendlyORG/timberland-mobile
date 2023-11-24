7672b5730a2776733a1bfd4f71f01b4f4ee6e6e4
EdisonModesto <edisonmodesto22@gmail.com>
2023-11-24 11:38:25 +0800

    updated faqs

--- lib/core/constants/faqs.dart
+++ lib/core/constants/faqs.dart
@@ -1,99 +1,135 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
-import 'package:flutter/material.dart';
-import 'package:timberland_biketrail/core/themes/timberland_color.dart';
 import 'package:timberland_biketrail/features/app_infos/domain/entities/faq.dart';
-import 'package:timberland_biketrail/features/app_infos/presentation/widgets/faq_diamond_icon.dart';
-import 'package:timberland_biketrail/features/app_infos/presentation/widgets/faq_icon_wrapper.dart';
 
 const faqs = [
   FAQ(
     question: "WHAT IS TMBP?",
     answer:
-        "Timberland Mountain Bike Park is a purpose-built network of trails located in the mountain township of Timberland Heights, developed by Filinvest. The trails are specific to mountain bikes and are graded according to difficulty.",
+        """Timberland Mountain BIke Park is a purpose-built network of mountain bike trails located in Timberland Heights. The Park is an extension and amenity of Timberland Highland Resort - much like MTB Parks abroad such as Whistler Blackcomb in Canada or Snowshoe in the USA.""",
   ),
   FAQ(
-    question: "WHAT ARE THE DIFFICULTY RATINGS?",
-    subCategory: [
-      FAQ(
-        question: "GRADE 1 (EASIEST)",
-        icon: FaqIconWrapper(
-          shape: BoxShape.circle,
-          icon: Icon(
-            Icons.circle,
-            color: Colors.white,
-          ),
-        ),
-        answer:
-            "Also identified by a white circle\n\nSuitable for novice riders, families and others seeking an easy, relaxing cycling experience. Trails are smooth, with only gentle climbs and generally wide enough for side-by-side riding.",
-      ),
-      FAQ(
-        question: "GRADE 2 (EASY)",
-        icon: FaqIconWrapper(
-          shape: BoxShape.circle,
-          icon: Icon(
-            Icons.circle,
-            color: Colors.green,
-          ),
-        ),
-        answer:
-            "Also identified by a green circle\n\nSuitable for most riders including beginners, occasional cyclists and families with limited cycling experience. A multi-geared bike with medium to wide knobbly tyres is recommended.\n\nTrails are usually wide and smooth with some gentle climbs. These trails are predictable, i.e. have no surprises.",
-      ),
-      FAQ(
-        question: "GRADE 3 (MORE DIFFICULT)",
-        icon: FaqIconWrapper(
-          icon: Icon(
-            Icons.square,
-            color: TimberlandColor.primary,
-          ),
-        ),
-        answer:
-            "Also identified with a blue square\n\nSuitable for regular experienced cyclists with a good level of fitness and over 12 years old; children should be accompanied by an adult.\n\nTrails can be narrow and may include hills, rollable drop-offs and small water crossings. Trail surfaces are mainly firm but may include muddy or loose sections, and obstacles such as rocks or tree roots.",
-      ),
-      FAQ(
-        question: "GRADE 4 (VERY DIFFICULT)",
-        icon: FaqDiamondIcon(
-          count: 1,
-        ),
-        answer:
-            "Also identified with a black diamond\n\nSuitable for fit, experienced cyclists with excellent off-road skills and a high level of fitness. Not suitable for children.\n\nA quality, well-maintained, full-suspension mountain bike is recommended. Trails are likely to be challenging with steep climbs and descents, as well as drops.",
-      ),
-      FAQ(
-        question: "GRADE 5 (EXTREMELY DIFFICULT)",
-        icon: FaqDiamondIcon(count: 2),
-        answer:
-            "Also identified with a double black diamond\n\nSuitable for fit, experienced cyclists with excellent off-road skills and a high level of fitness. Not suitable for children.\n\nA well-maintained, full-suspension mountain bike is recommended, features will be more technical and difficult than a Grade 4 trail. A full-face helmet and additional body protection is recommended.",
-      ),
-    ],
-  ),
-  FAQ(
-    question: "HOW DO I GET TO RIDE TMBP?",
+    question: "How much is the park entrance fee?",
     answer:
-        "Using this app you can book an available date and also pay for your day pass. The day pass lets you ride as much as you want on your selected date.",
+        "The Park fee is P550.00 for a day pass. This means you can enter and exit Timberland Mountain Bike Park several times within the day. The day pass also includes personal accident insurance.",
   ),
   FAQ(
-    question: "CAN I RIDE TMBP IN THE RAIN?",
+    question: "What are TMBP’s operating hours?",
     answer:
-        "Due to potential trail damage as well as risks to riders, management reserves the right to restrict access to the trails during inclement weather.",
+        "Initially, TMBP will be open on weekends only. The Park opens at 7:30am, and closes at 4:00pm. The cutoff for entry during the afternoon is 2:00pm. Operating hours and days may change as the park grows.",
   ),
   FAQ(
-    question: "CAN I CANCEL MY BOOKING?",
+    question: "What’s the minimum age to ride in TMBP?",
     answer:
-        "While it makes us sad, you may cancel your ride booking up to 48H before your scheduled ride. Cancelled bookings will be credited to your account for a future ride, redeemable within the next 4 months. We miss you that much!",
+        "For now, the minimum age for a rider is 16 years old. Riders 18 years old and below need to be accompanied by an adult guardian or parent. All riders must sign a waiver upon registration.",
   ),
   FAQ(
-    question: "WHAT IF I DON'T SHOW UP DURING MY SCHEDULED RIDE?",
+    question: "How do I register to ride?",
     answer:
-        "No-shows are considered as canceled rides, but your account will be credited with a future ride, which you can redeem/rebook within the next 4 months. We hope to see you soon!",
+        "To register, riders need to visit the TMBP Office at Timberland Highlands Resort. Riders are required to fill up and sign a waiver before purchasing a day pass. Riders will then be issued a wrist tag for the day.",
   ),
   FAQ(
-    question: "WHAT IF I SHOW UP WITHOUT A BOOKING?",
+    question: "I’m a first-time visitor to TMBP - any tips?",
     answer:
-        "Since slots are limited, all bookings will have to be done using the app. We hope you understand.",
+        "Upon registration, please indicate that you are a first time visitor. First timers will need to undergo a short briefing before entering the trails. As with riding anything new, it is best to do so conservatively to get familiar with the trails and our trail features.",
   ),
   FAQ(
-    question:
-        "WHAT IF I DON’T HAVE A SMARTPHONE? HOW CAN I BOOK WITHOUT THE APP?",
-    answer: "Please contact TMBP management via XXXXXXXXX.",
+    question: "What trails should I start with?",
+    answer:
+        "From our current trail network, we recommend Skyline and the two trails in the Skills Zone: Fifty First Dates and Ground Hog Day. These will help riders warm up and get familiar with the different features they will encounter in the trails. From there you can go up to Launch Pad, and ride down Point Break.",
   ),
+  FAQ(
+    question: "What types of trails are there?",
+    answer:
+        "We designed TMBP to accommodate riders of different skill levels. As such, we have trail ratings relevant to the riders’ skill level, with corresponding color codes. White trails are for Novice riders. Green trails are for Intermediate riders. Blue trails are for Advanced riders. Black trails are for Expert riders. We recommend riders take time to practice their skills at the Skills Zone before doing any of our Blue or Black trails. Please ride within your ability.",
+  ),
+  FAQ(
+    question: "Where can I get refreshments?",
+    answer:
+        "We recommend that all trail riders be as self-sufficient as possible. Should you run out of water or snacks, there is a lemonade stand located at the Skills Zone.",
+  ),
+  FAQ(
+    question: "What should I wear?",
+    answer:
+        "A certified MTB helmet is a must. Comfortable (or technical) cycling apparel, gloves, protective eyewear, and closed riding shoes are recommended. Hydration (either water bottles or hydration pack), along with a multitool, tire levers, spare tube and an inflation system (pump) is also a standard loadout for a trail ride.",
+  ),
+  FAQ(
+    question: "What bike should I use?",
+    answer:
+        "We recommend, at the very least, a hardtail MTB with at least 100mm of front suspension, knobby tires that are 2.0 inches wide or wider. A dropper post is also helpful in technical sections.",
+  ),
+  FAQ(
+    question: "Can I use my gravel bike? Cyclocross bike?",
+    answer:
+        "While you certainly can use these bikes on our trails, these are not ideal and not recommended - especially from a safety standpoint. Different traction levels and braking characteristics may make the trails especially challenging. Also keep in mind that you will be sharing the trails with other riders. Safety is everybody’s responsibility.",
+  ),
+  FAQ(
+    question: "Can I use an e-MTB?",
+    answer: "Yes, PEDAL ASSIST e-MTBs are allowed. Throttle-based electric bikes are not.",
+  ),
+  FAQ(
+    question: "Can I bring my trail dog?",
+    answer:
+        "As much as we love our four-legged furry friends, the safety of all riders is our prime consideration, so for now pets are not allowed inside the Park.",
+  ),
+  FAQ(
+    question: "Can I run on the trails?",
+    answer:
+        "TMBP was designed for mountain bikes, as such, the features and flow of the trails are optimized for MTB. TMBP is exclusive to mountain bikes. Trail runners and hikers are not allowed on the trails.",
+  ),
+  FAQ(
+    question: "What’s the total mileage of the trails?",
+    answer:
+        "As of now, TMBP has around 16km of purpose-built mountain bike trails. Note that some trails, particularly Bill & Ted, are used to loop back to the start - so your potential ride mileage may vary.",
+  ),
+  FAQ(
+    question: "Can I ride the trails at night?",
+    answer:
+        "Sorry, but night riding is not allowed. The cutoff for entry during the afternoon is 2:00pm, and the Park closes at 4:00pm. All riders must exit the park at 4:00pm. Our Bike Park Patrol will ensure that everybody complies.",
+  ),
+  FAQ(
+    question: "Can I exit the Park, have a snack at the resort, then come back?",
+    answer: "Yes! The day pass entitles you to enter and exit the Park multiple times within Park operating hours.",
+  ),
+  FAQ(
+    question: "How do I exit the Park?",
+    answer:
+        "From Launch Pad, head to Skills Zone via Fifty First Dates or Ground Hog Day. After the last berm of either trail you will see a sign on your right that says Park Exit Trail. Follow this trail down to the Exit - but we expect to see you on the trails again soon!",
+  ),
+  FAQ(
+    question: "Do you have bike rental services?",
+    answer: "Bike rental is something that is being considered as a long-term plan. But for now, these are not yet available.",
+  ),
+  FAQ(
+    question: "Do you have bike wash services?",
+    answer:
+        "Yes we do. We have coin-operated self-service bike wash stations at the lower lobby of Timberland Highlands Resort, and also at Parking Lot 5.",
+  ),
+  FAQ(
+    question: "Where do I park my car?",
+    answer:
+        "When you enter Timberland Highlands Resort, Parking Lots 4 and 5 are exclusive for TMBP riders. The guard at the entrance will happily direct you.",
+  ),
+  FAQ(
+    question: "Do you offer season passes?",
+    answer:
+        "For now, season passes are bundled with the rest of the resort’s amenities. Visit the TMBP office or the Timberland Highlands Resort lobby and ask about their Peak Plans.",
+  ),
+  FAQ(
+    question: "Does the Resort offer room packages that include Park access?",
+    answer: "For now these packages are not yet implemented, but eventually yes there will be bundles made available.",
+  ),
+  FAQ(
+    question: "What if I get lost on the trail?",
+    answer:
+        "The trails in TMBP all flow in one direction, and any forks are clearly marked with indicators. The Park also has an extensive signage system which makes navigation intuitive.",
+  ),
+  FAQ(
+    question: "Can I ride TMBP alone?",
+    answer: "Yes you can, But mountain biking is more fun when shared with friends.",
+  ),
+  FAQ(
+      question: 'What else can we expect in the future?',
+      answer:
+          "Aside from more features and more trails, we are always looking for ways to improve the Park and resort experience. We have a mobile app in the works that will contain not just Park information, but will enable riders to book and purchase day passes in advance. ")
 ];
