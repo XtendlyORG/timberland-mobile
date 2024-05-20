82f347c45ab6a2257479f3a29d89bda2f56b470e
Charles Bon Chua <chuacharlesbon@gmail.com>
2024-05-20 08:15:26 +0800

    Updated: Temp Model Adjustment

--- lib/features/authentication/data/models/user_model.dart
+++ lib/features/authentication/data/models/user_model.dart
@@ -31,26 +31,26 @@ class UserModel extends User {
     );
     return UserModel(
       id: (map['member_id'] as num?).toString(),
-      prettierID: map['long_id'] as String,
-      firstName: map['firstname'] as String,
+      prettierID: map['long_id'] != null ? map['long_id'] as String : "",
+      firstName: map['firstname'] != null ? map['firstname'] as String : "",
       middleName:
           map['middlename'] != null ? map['middlename'] as String : null,
-      lastName: map['lastname'] as String,
-      profilePicUrl: map['profile_pic'] as String? ?? '',
-      gender: map['gender'] as String,
-      birthday: DateTime.tryParse(map['birth_date'] as String? ?? ''),
+      lastName: map['lastname'] != null ? map['lastname'] as String : "",
+      profilePicUrl: map['profile_pic'] != null ? map['profile_pic'] as String : '',
+      gender: map['gender'] != null ? map['gender'] as String : "",
+      birthday: map['birth_date'] != null ? DateTime.tryParse(map['birth_date'] as String? ?? '') : DateTime.now(),
       address: map['address'] != null ? map['address'] as String : null,
       profession:
           map['profession'] != null ? map['profession'] as String : null,
       bloodType: map['blood_type'] != null ? map['blood_type'] as String : null,
-      email: map['email'] as String,
-      mobileNumber: map['mobile_number'] as String,
+      email: map['email'] != null ? map['email'] as String : "",
+      mobileNumber: map['mobile_number'] != null ? map['mobile_number'] as String : "",
       bikeModel: map['bike_model'] != null ? map['bike_model'] as String : null,
       bikeYear: map['bike_year'] != null
           ? (map['bike_year'] as num).toString()
           : null,
       bikeColor: map['bike_color'] != null ? map['bike_color'] as String : null,
-      accessCode: map['access_code'] as String,
+      accessCode: map['access_code'] != null ? map['access_code'] as String : "",
       emergencyContactInfo: map['emergency_number'] != null
           ? map['emergency_number'] as String
           : null,
